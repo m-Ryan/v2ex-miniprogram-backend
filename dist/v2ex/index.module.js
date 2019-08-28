78897c6d27c4cccf28570429eea28cfbbb05ae88
毛灿华 <canhua.mao@lizhiweike.com>
2019-08-28 14:27:35 +0800

    update

--- dist/v2ex/index.module.js
+++ dist/v2ex/index.module.js
@@ -5,18 +5,10 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("@nestjs/common");
 const index_controller_1 = require("./index.controller");
 const index_service_1 = require("./index.service");
-const redisStore = __importStar(require("cache-manager-redis-store"));
 let V2exModule = class V2exModule {
 };
 V2exModule = __decorate([
@@ -25,7 +17,6 @@ V2exModule = __decorate([
             common_1.CacheModule.register({
                 ttl: 60,
                 max: 300,
-                store: redisStore,
             }),
         ],
         controllers: [index_controller_1.V2exController],
--- src/v2ex/index.module.ts
+++ src/v2ex/index.module.ts
@@ -1,13 +1,11 @@
 import { Module, CacheModule } from '@nestjs/common';
 import { V2exController } from './index.controller';
 import { V2exService } from './index.service';
-import * as redisStore from 'cache-manager-redis-store';
 @Module({
   imports: [
     CacheModule.register({
       ttl: 60, // seconds
       max: 300, // seconds
-      store: redisStore,
     }),
   ],
   controllers: [V2exController],
