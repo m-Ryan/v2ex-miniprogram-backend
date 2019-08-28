5ad2b5d6c24cdf1077f4a20938f59c0acace0a61
毛灿华 <canhua.mao@lizhiweike.com>
2019-08-28 16:18:12 +0800

    user redisStore

--- src/v2ex/index.module.ts
+++ src/v2ex/index.module.ts
@@ -1,12 +1,12 @@
 import { Module, CacheModule } from '@nestjs/common';
 import { V2exController } from './index.controller';
 import { V2exService } from './index.service';
-import memcachedStore from 'cache-manager-memcached-store';
+import * as redisStore from 'cache-manager-redis-store';
 @Module({
   imports: [CacheModule.register({
     ttl: 60, // seconds
     max: 300, // seconds
-    store: memcachedStore
+    store: redisStore
   })],
   controllers: [V2exController],
   providers: [V2exService],
