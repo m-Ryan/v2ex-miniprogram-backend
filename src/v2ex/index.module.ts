de8502ca072af8df55f7dab15055e7a0aafc8820
毛灿华 <canhua.mao@lizhiweike.com>
2019-08-28 16:23:21 +0800

    update

--- src/v2ex/index.module.ts
+++ src/v2ex/index.module.ts
@@ -1,12 +1,20 @@
 import { Module, CacheModule } from '@nestjs/common';
 import { V2exController } from './index.controller';
 import { V2exService } from './index.service';
-import * as redisStore from 'cache-manager-redis-store';
+import * as fsStore from 'cache-manager-fs-binary';
 @Module({
   imports: [CacheModule.register({
     ttl: 60, // seconds
     max: 300, // seconds
-    store: redisStore
+    store: fsStore,
+    options: {
+      reviveBuffers: true,
+      binaryAsStream: true,
+      ttl: 60 /* seconds */,
+      maxsize: 1000 * 1000 * 1000 /* max size in bytes on disk */,
+      path: 'diskcache',
+      preventfill: true
+  }
   })],
   controllers: [V2exController],
   providers: [V2exService],
