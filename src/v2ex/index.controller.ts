e886a5a0d6becc4c8e750a7843f1e8c0f0dabecb
m-Ryan <962491243@qq.com>
2019-10-28 23:05:49 +0800

    set cookie

--- src/v2ex/index.controller.ts
+++ src/v2ex/index.controller.ts
@@ -8,13 +8,29 @@ import {
   Headers,
 } from '@nestjs/common';
 import { V2exService } from './index.service';
-import { BASE_URL, MOCK_COOKIE } from '../constants';
+import { BASE_URL } from '../constants';
 import { UserError } from '../common/filters/userError';
+
+let MOCK_COOKIE = `__cfduid=d42f7d8d9896ee16dae8e60a522124ae31571754617; _ga=GA1.2.986388969.1571754618; A2="2|1:0|10:1571754997|2:A2|48:YzE4ZDRkMGYtNzFlYi00OTNhLTk5MWItNzlmYjQxNjgyNGNl|0e8c1d024db8b13acbc8dcb441a5c246ac546bf9fc179b895eb1aedb8e4cb68f"; V2EX_LANG=zhcn; V2EX_REFERRER="2|1:0|10:1572186312|13:V2EX_REFERRER|12:YXB0eDQ2ODk=|7a284743314ee58e52399928cda5df9f45f736e3c36218232d28de0249a61171"; V2EX_TAB="2|1:0|10:1572186381|8:V2EX_TAB|8:dGVjaA==|25f3bc36631da8fec3b2c7644f573baa613cd4b162fd6ff8a3be36133a37aecc"; PB3_SESSION="2|1:0|10:1572274792|11:PB3_SESSION|36:djJleDo2OC40LjIwMy4xMzE6OTMwODU2MzY=|86ad7fb40a3d29d63b807b0c8380a7cb8051219a94ba5d880ae4275645b34ea0"; _gid=GA1.2.1633480721.1572274793; _gat=1"; V2EX_LANG=zhcn; _gid=GA1.2.258513727.1570441028`;
 
 @Controller('v2ex')
 @UseInterceptors(CacheInterceptor)
 export class V2exController {
   constructor(private readonly service: V2exService) { }
+
+  @Get('set-mock-cookie')
+  async setMockCookie(@Query('cookie') cookie: string) {
+    if (!cookie) {
+      return;
+    }
+    MOCK_COOKIE = cookie;
+    return MOCK_COOKIE;
+  }
+
+  @Get('get-mock-cookie')
+  async getMockCookie() {
+    return MOCK_COOKIE;
+  }
 
   @Get('tab')
   async getHome(@Query('tab') tab: string = 'tech', @Headers('v2ex-cookie') cookie: string = MOCK_COOKIE) {
